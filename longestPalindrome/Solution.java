81d77ec145e021a5a93fa642c34d5940127397ae
Daphne Li-Chen <daphneli-chen@daphnes-mbp.myfiosgateway.com>
2020-09-17 17:39:29 -0400

    longest palindrome

---
+++ longestPalindrome/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(i, i, s);
+            int len2 = expandAroundCenter(i, i+1, s);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+    
+    public int expandAroundCenter(int left, int right, String s) {
+        int L = left;
+        int R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+}
